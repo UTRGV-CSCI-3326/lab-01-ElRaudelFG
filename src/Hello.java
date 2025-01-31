6b1a4b22fcd15f88314cb843efd3ee5437c4dcec
ElRaudelFG <157167825+ElRaudelFG@users.noreply.github.com>
2025-01-31 21:38:32 +0000

    First commit

--- src/Hello.java
+++ src/Hello.java
@@ -1,1 +1,9 @@
 // Hello World
+public class Hello {
+
+    public static void main(String[] args) { //work space
+
+        // cout << "Hello World!";
+    System.out.println("Hello World!");
+    }
+}
